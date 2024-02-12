89683365ecbe482df2725ff4c59866b6ae6cf2c5
Dev-Istyak <148707940+Dev-Istyak@users.noreply.github.com>
2024-02-12 16:28:47 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const myCheckbox=document.getElementById("myCheckbox");
+const visaBtn=document.getElementById("visaBtn");
+const masterBtn=document.getElementById("masterBtn");
+const paypalBtn=document.getElementById("paypalBtn");
+const mysubmit=document.getElementById("mysubmit");
+const subresult=document.getElementById("subresult");
+const paymentresult=document.getElementById("paymentresult");
+
+mysubmit.onclick=function(){
+    if(myCheckbox.checked){
+        subresult.textContent=`you are subscribed!`;
+    }
+    else{
+        subresult.textContent=`you are not subscribed yet`;
+    }
+    if(visaBtn.checked){
+        paymentresult.textContent=`you are pauing with visa`;
+    }
+    else if(masterBtn.checked){
+        paymentresult.textContent=` you are paying with master card`;
+
+    }
+    else if(paypalBtn.checked){
+        paymentresult.textContent=`you are paying with paypal`;
+
+    }
+    else{
+        paymentresult.textContent=`you have to choose a payment mathod`;
+    }
+
+}
